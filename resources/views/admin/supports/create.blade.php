a161ba0a2bd1d1ad285a86eb58b9831330c2e4ba
Allyster <allystercosta@gmail.com>
2024-01-04 11:55:49 -0300

    Frist Commit

---
+++ resources/views/admin/supports/create.blade.php
@@ -0,0 +1,6 @@
+<h1>Nova dúvida</h1>
+
+<form action="" method="POST">
+    <input type="text" placeholder="Assunto" name="subject">
+    <textarea name="body" cols="30" rows="5" placeholder="Descrição"></textarea>
+</form>
